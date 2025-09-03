9f30b0ef3e3d3152b108115caad689dd80df7caa
Bhavin-Viramgama <abhavin31@gmail.com>
2025-09-03 11:31:55 +0530

    Minor changes

--- Unit Converter/app/src/main/java/com/example/unitconverter/MainActivity.kt
+++ Unit Converter/app/src/main/java/com/example/unitconverter/MainActivity.kt
@@ -22,6 +22,7 @@ import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -228,7 +229,8 @@ fun CreditText() {
         withStyle(
             style = SpanStyle(
                 fontWeight = FontWeight.Bold,
-                fontFamily = FontFamily.Monospace
+                fontFamily = FontFamily.Monospace,
+                textDecoration = TextDecoration.Underline
             )
         ) {
             append("Bhavin")
